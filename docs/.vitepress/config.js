a5f4ce6ea6bc06e4b610af5423beebc89d7705f3
小倉優悟 <jigenbakdaaan@gmail.com>
2020-10-11 19:28:29 +0900

    fix locale config

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,112 +1,121 @@
 module.exports = {
-  title: 'VitePress Local Playground',
+  title: "VitePress Local Playground",
   locales: {
-    '/': {
-      label: 'English',
-      lang: 'en-US',
-      title: 'VitePress Playground',
-      description: 'Vite-powered Static Site Generator'
+    "/": {
+      lang: "en-US",
+      title: "VitePress Playground",
+      description: "Vite-powered Static Site Generator",
     },
-    '/zh/': {
-      label: '中文',
-      lang: 'zh-CN',
-      title: 'VitePress 游乐场',
-      description: 'Vite 驱动的静态网站生成器'
+    "/zh/": {
+      lang: "zh-CN",
+      title: "VitePress 游乐场",
+      description: "Vite 驱动的静态网站生成器",
     },
-    '/ja/': {
-      label: '日本語',
-      lang: 'ja-JP',
-      title: 'VitePress プレイグラウンド',
-      description: 'Vite 静的サイトジェネレーター！'
+    "/ja/": {
+      lang: "ja-JP",
+      title: "VitePress プレイグラウンド",
+      description: "Vite 静的サイトジェネレーター！",
     },
   },
 
   themeConfig: {
+    repo: "vuejs/vuex",
     nextLinks: true,
     prevLinks: true,
 
     nav: [
-      { text: 'Guide', items: [
-        { text: 'First', link: '/guide/nested/', },
-        { text: 'Second', link: '/guide/getting-started', },
-        { text: 'Third', link: '/guide/guide-second', },
-      ]
-      } ,
-      { text: 'Nested', link: '/guide/nested/', },
+      {
+        text: "Guide",
+        items: [
+          { text: "First", link: "/guide/nested/" },
+          { text: "Second", link: "/guide/getting-started" },
+          { text: "Third", link: "/guide/guide-second" },
+        ],
+      },
+      { text: "Nested", link: "/guide/nested/" },
     ],
 
     sidebar: {
-      '/': [
-        { text: 'Index', link: 'index' },
-      ],
+      "/": [{ text: "Index", link: "index" }],
 
-      '/guide/': [
+      "/guide/": [
         {
-          text: 'Prologue',
+          text: "Prologue",
           children: [
-            { text: 'Index', link: 'guide/index' },
-            { text: 'Getting Started', link: 'guide/getting-started' },
-            { text: 'Guide Second', link: 'guide/guide-second' },
-          ]
+            { text: "Index", link: "guide/index" },
+            { text: "Getting Started", link: "guide/getting-started" },
+            { text: "Guide Second", link: "guide/guide-second" },
+          ],
         },
       ],
 
-      '/guide/nested/': [
+      "/guide/nested/": [
         {
-          text: 'Nested',
+          text: "Nested",
           children: [
-            { text: 'Nested Index', link: 'guide/nested/index' },
-            { text: 'Nested Something', link: 'guide/nested/something' }
-          ]
-        }
+            { text: "Nested Index", link: "guide/nested/index" },
+            { text: "Nested Something", link: "guide/nested/something" },
+          ],
+        },
       ],
     },
 
     locales: {
-      '/zh/': {
+      "/": {
+        label: "English",
+        selectText: "Languages",
+      },
+      "/zh/": {
+        label: "中文",
+        selectText: "选择语言",
         nav: [
-          { text: '指南', link: '/zh/guide/' },
-          { text: 'API', link: '/zh/api/' },
-          { text: '更新日志', link: 'https://github.com/vuejs/vuex/releases' },
+          { text: "指南", link: "/zh/guide/" },
+          { text: "API", link: "/zh/api/" },
+          { text: "更新日志", link: "https://github.com/vuejs/vuex/releases" },
         ],
         sidebar: [
           {
-            text: '指南',
+            text: "指南",
             children: [
-              { text: '指南', link: '/zh/guide/' },
-              { text: '快速开始', link: '/zh/guide/getting-started' },
-              { text: '指南内容', link: '/zh/guide/guide-content' },
-              { text: '内容', link: '/zh/guide/content' },
+              { text: "指南", link: "/zh/guide/" },
+              { text: "快速开始", link: "/zh/guide/getting-started" },
+              { text: "指南内容", link: "/zh/guide/guide-content" },
+              { text: "内容", link: "/zh/guide/content" },
             ],
           },
           {
-            text: '内容',
-            children: [{ text: '内容', link: '/zh/guide/content' }],
+            text: "内容",
+            children: [{ text: "内容", link: "/zh/guide/content" }],
           },
         ],
       },
-      '/ja/': {
+      "/ja/": {
+        label: "日本語",
+        selectText: "言語",
         nav: [
-          { text: 'ガイド', link: '/ja/guide/' },
-          { text: 'API', link: '/ja/api/' },
-          { text: 'リリースノート', link: 'https://github.com/vuejs/vuex/releases' },
+          { text: "ガイド", link: "/ja/guide/" },
+          { text: "API", link: "/ja/api/" },
+          {
+            text: "リリースノート",
+            link: "https://github.com/vuejs/vuex/releases",
+          },
         ],
         sidebar: [
           {
-            text: 'ガイド',
+            text: "ガイド",
             children: [
-              { text: 'ガイド', link: '/ja/guide/' },
-              { text: 'はじめる', link: '/ja/guide/getting-started' },
-              { text: 'ガイドの内容', link: '/ja/guide/guide-content' },
-              { text: '内容', link: '/ja/guide/content' },
+              { text: "ガイド", link: "/ja/guide/" },
+              { text: "はじめる", link: "/ja/guide/getting-started" },
+              { text: "ガイドの内容", link: "/ja/guide/guide-content" },
+              { text: "内容", link: "/ja/guide/content" },
             ],
           },
           {
-            text: '内容',
-            children: [{ text: '内容', link: '/ja/guide/content' }],
+            text: "内容",
+            children: [{ text: "内容", link: "/ja/guide/content" }],
           },
         ],
-      }
-    }
-  }
-}
+      },
+    },
+  },
+};
